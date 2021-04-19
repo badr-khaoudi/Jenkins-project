29b79ea3a84f42a500a0c5bba198a300a4df607f
badr-khaoudi <b.khaoudi@inspheris.com>
2021-04-19 00:15:23 +0000

    Test Spring app in jenkins

---
+++ src/main/java/com/example/TestJenkins/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.TestJenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@GetMapping("/")
+	public String Hello() {
+		
+		return "Hello World ! ";
+	}
+
+}
