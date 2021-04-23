3d591a355b5be43371de7b2009ad58be87d7e394
badr-khaoudi <80256769+badr-khaoudi@users.noreply.github.com>
2021-04-23 02:29:45 +0000

    Update TestJenkinsApplication.java

--- src/main/java/com/example/TestJenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/TestJenkins/TestJenkinsApplication.java
@@ -14,6 +14,8 @@ public class TestJenkinsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World!");
+	
+		sssss
 	}
 
 }
